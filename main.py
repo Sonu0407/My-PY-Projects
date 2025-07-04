4007b31bd0924bc28d38327cacf221a0b610d7d7
Sonu0407 <sonu.sapri2004@gmail.com>
2025-07-04 12:23:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,87 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrian
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
+import pyperclip
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-question_bank = []
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(q_text = question_text, q_answer = question_answer)
-    question_bank.append(new_question)
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-quiz = QuizBrian(question_bank)
-while quiz.still_has_question():
-    quiz.next_question()
+    password_list = []
 
-print("You have completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+    for char in range(nr_letters):
+      password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+      password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+      password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+      password += char
+
+    print(f"Your password is: {password}")
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password):
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty..")
+
+    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
+                                                  f"\nPassword: {password} \nIs it ok to save?")
+    if is_ok:
+        with open("data.txt", "a") as data_file:
+            data_file.write(f"{website} | {email} | {password}\n")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+# ---------------------------- UI SETUP ------------------------------- #
+from tkinter import *
+from tkinter import messagebox
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image = logo_img)
+canvas.grid(column=1, row=0)
+
+website_name = Label(text="Website", font=("Times New Roman", 10))
+website_name.grid(column=0, row=1)
+email_username = Label(text="Email/Username", font=("Times New Roman", 10))
+email_username.grid(column=0, row=2)
+password_label = Label(text="Password", font=("Times New Roman", 10))
+password_label.grid(column=0, row=3)
+
+#Entries (input field)
+website_entry = Entry(width=45)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=45)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "sonu.s@gmail.com")
+password_entry = Entry(width=45)
+password_entry.grid(column=1, row=3, columnspan=2)
+
+# Buttons
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(column=2, row=3)
+add_button = Button(text="Add", width=38, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
