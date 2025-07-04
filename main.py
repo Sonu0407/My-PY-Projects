cc7d03f1ad751f52255ad62d3897b532f4aa2a79
Sonu0407 <sonu.sapri2004@gmail.com>
2025-07-04 12:23:54 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,87 +1,56 @@
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 import random
-import pyperclip
-def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-    nr_letters = random.randint(8, 10)
-    nr_symbols = random.randint(2, 4)
-    nr_numbers = random.randint(2, 4)
-
-    password_list = []
-
-    for char in range(nr_letters):
-      password_list.append(random.choice(letters))
-
-    for char in range(nr_symbols):
-      password_list += random.choice(symbols)
-
-    for char in range(nr_numbers):
-      password_list += random.choice(numbers)
-
-    random.shuffle(password_list)
-
-    password = ""
-    for char in password_list:
-      password += char
-
-    print(f"Your password is: {password}")
-    password_entry.insert(0, password)
-    pyperclip.copy(password)
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-def save():
-    website = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
-
-    if len(website) == 0 or len(password):
-        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty..")
-
-    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
-                                                  f"\nPassword: {password} \nIs it ok to save?")
-    if is_ok:
-        with open("data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}\n")
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
-
-# ---------------------------- UI SETUP ------------------------------- #
 from tkinter import *
-from tkinter import messagebox
+import pandas
+BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+def next_card():
+    global current_card
+    global flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image = logo_img)
-canvas.grid(column=1, row=0)
+flip_timer = window.after(3000, func=flip_card)
 
-website_name = Label(text="Website", font=("Times New Roman", 10))
-website_name.grid(column=0, row=1)
-email_username = Label(text="Email/Username", font=("Times New Roman", 10))
-email_username.grid(column=0, row=2)
-password_label = Label(text="Password", font=("Times New Roman", 10))
-password_label.grid(column=0, row=3)
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "bold"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, row=0, columnspan=2)
 
-#Entries (input field)
-website_entry = Entry(width=45)
-website_entry.grid(column=1, row=1, columnspan=2)
-website_entry.focus()
-email_entry = Entry(width=45)
-email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, "sonu.s@gmail.com")
-password_entry = Entry(width=45)
-password_entry.grid(column=1, row=3, columnspan=2)
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, command=next_card)
+unknown_button.config(highlightthickness=0)
+unknown_button.grid(column=0, row=1)
 
-# Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(column=2, row=3)
-add_button = Button(text="Add", width=38, command=save)
-add_button.grid(column=1, row=4, columnspan=2)
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, command=next_card)
+known_button.config(highlightthickness=0)
+known_button.grid(column=1, row=1)
+
+next_card()
+
 
 window.mainloop()
+
